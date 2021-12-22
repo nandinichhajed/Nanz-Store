35ad338d2329d2d6ae53e1294c65be4013ef8b56
nandinichhajed <nandinichhajed08@gmail.com>
2021-12-22 22:49:52 +0530

    creating serializers for user

--- ecom/api/user/serializers.py
+++ ecom/api/user/serializers.py
@@ -5,9 +5,30 @@ from rest_framework.decorators import authentcation_classes, permission_classes
 from .models import CustomUser
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-        class Meta:
-            model = CustomUser
-            fields = ('name', 'email', 'password', 'phone', 'gender', 'is_active', 'is_staff', 'is_superuser')
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        for artt, value in validate_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+                
+        instance.save()
+        return instance
+    
+    class Meta:
+        model = CustomUser
+        extra_kwargs = {'password': {'write-only': True}}
+        fields = ('name', 'email', 'password', 'phone', 'gender', 'is_active', 'is_staff', 'is_superuser')
             
     
     
