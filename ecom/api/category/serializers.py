292e5ae919272636f519fce9b5b21b06e084eef1
nandinichhajed <nandinichhajed08@gmail.com>
2021-12-22 17:13:56 +0530

    updating category serializers

--- ecom/api/category/serializers.py
+++ ecom/api/category/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Category
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    class   Meta:
+    class Meta:
         model = Category
         fields = ('name', 'description')
