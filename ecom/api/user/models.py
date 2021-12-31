52ca8673d6ff1e46613cd2fc7dc61dbb15b6bbff
nandinichhajed <nandinichhajed08@gmail.com>
2021-12-31 22:42:04 +0530

    adding comments in models file

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomeUser(AbstractUser):
-    name = models.CharField(max_length=50, default='Anonymous')
+    name = models.CharField(max_length=50, default='Anonymous')  
+    # if no name is given we ca write default name as Anonymous
     email = models.EmailField(max_length=254, unique=True)
     
     username = None
@@ -16,6 +17,8 @@ class CustomeUser(AbstractUser):
     gender = models.CharField(max_length=50, blank=True, null=True)    
 
     session_token = models.CharField(max_length=10, default=0)
+    # default=0 means that the user is not loggined
+    
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
