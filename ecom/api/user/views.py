1e59b749e4667def0705837feedde34d2a75870b
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-05 18:10:20 +0530

    correcting typos

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -75,6 +75,7 @@ class UserViewSet(viewsets.ModelViewSet):
     
     def get_permissions(self):
         try:
-            return [permission() for permission in self.permission_classes_by_action(self.action)]
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        
         except KeyError:
             return [permission() for permission in self.permission_classes]
