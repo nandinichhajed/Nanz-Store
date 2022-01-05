157009a39b55c7aa1933d145cc823e0c2a047275
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-05 16:33:32 +0530

    correcting typos

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class CustomeUser(AbstractUser):
+class CustomUser(AbstractUser):
     name = models.CharField(max_length=50, default='Anonymous')  
     # if no name is given we ca write default name as Anonymous
     email = models.EmailField(max_length=254, unique=True)
--- ecom/api/user/serializers.py
+++ ecom/api/user/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
-from rest_framework.decorators import authentcation_classes, permission_classes
+from rest_framework.decorators import authentication_classes, permission_classes
 
 from .models import CustomUser
 
