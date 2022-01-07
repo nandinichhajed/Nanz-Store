38537d376c72c8c7bb7b36fd054ae099bc01b9d5
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-07 14:09:16 +0530

    created serializers file

---
+++ ecom/api/order/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Order
+
+class OrderSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Order
+        fields = ('user')
+        # fields = ('user', 'product_name', 'total_product')
+        # TODO: add product and quantity
+        
