1f3386636c188fcd8d5c52a4dff28bee4f22e080
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-20 15:22:19 +0530

    customizing admin pannel

--- ecom/api/user/admin.py
+++ ecom/api/user/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    list_display_links = ('id', 'name')
+
+admin.site.register(CustomUser, CustomUserAdmin)
