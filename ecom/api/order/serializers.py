50e397cadf2893d06955331ed9e10d7042ed4e8a
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-20 15:22:03 +0530

    correcting typo

--- ecom/api/order/serializers.py
+++ ecom/api/order/serializers.py
@@ -4,7 +4,7 @@ from .models import Order
 class OrderSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
-        fields = ('user')
+        fields = ('user',)
         # fields = ('user', 'product_name', 'total_product')
         # TODO: add product and quantity
         
